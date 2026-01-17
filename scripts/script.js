aa7e6d7eb85342f2893ec2cf7bcd265c5bc26af7
ezschitown <ezschitown@gmail.com>
2026-01-16 19:15:20 -0600

    testing the submit button

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+//all the setOrg vars are for the org catagories and hold the values in a array like obj
+const acceptSub = document.querySelector("[suggestion-but]")
+const clearItems = document.querySelectorAll("[clear-sub]")
+const changeItems = document.querySelectorAll("[change-sub]")
+
+//ex org1 set to last index 
+//waits for someone to type into the thing and gets input
+acceptSub.addEventListener("click",(e)=>{
+    for(let i=0;i< clearItems.length;i++ ){
+        clearItems[i].classList.toggle("hide", true)
+    }
+    changeItems[0].textContent = "thank"
+    changeItems[1].textContent ="yea"
+})
