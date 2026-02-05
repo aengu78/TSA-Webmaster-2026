b1c96cb50c707e5b5dbc3ab07d07265033d3b690
aengu78 <reimxnade@gmail.com>
2026-02-04 21:10:45 -0600

    added some mobile functionality

---
+++ scripts/mobile_nav.js
@@ -0,0 +1,5 @@
+function show() {
+  document.getElementById('mobile-nav').classList.toggle('active');
+  document.getElementById('nav-opener').classList.toggle('active');
+  console.log("showing");
+}
